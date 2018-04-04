6cee7397c24219e84e9081dcc2cff875ca9f5770
Ge Gao <ge.gao@ef.com>
2018-04-03 23:16:18 -0400

    new repo moving

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -4,15 +4,13 @@ import breeze.optimize._
 
 object Main extends App {
   
-  Crawl.greetFromCrawl()
-  TopicModel.greetFromTopicModel()
-
-
-  var matrix = TopicModel.term_by_document_matrix(List("The dog chewed my shoes", 
-                                                       "The cat scratched my chair",
-                                                       "the mouse chewed my cereal"))
-
-
+  /*******************   Testing term by document matrix   ********************/ 
+  var testDocuments = List("The dog chewed my shoes", 
+                           "The cat scratched my chair",
+                           "the mouse chewed my cereal")
+  var matrix = TopicModel.term_by_document_matrix(testDocuments)
   println(matrix)
 
+
+  
 }
