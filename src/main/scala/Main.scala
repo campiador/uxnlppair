8e345c999dd654ca64fb9ca83dc0647e39a9637d
Ge Gao <ge.gao@ef.com>
2018-05-09 21:49:32 -0400

    getTermByNumber & getDocumentByNumber

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -10,33 +10,37 @@ object Main extends App {
   // choose k = number of topics here
   val reduced_rank = 10
 
-
    
   /*******************   Testing term by document matrix   ********************/ 
-//  var testDocuments = List("the cat sat on my face",
-//                           "the dog sat on my bed")
-//  var matrix = TopicModel.term_by_document_matrix(testDocuments)
-////  println(matrix)
-//  var tfidfMatrix = TopicModel.tf_idf_scores(matrix, testDocuments)
-//  var str = TopicModel.create_object_for_LSA_model(tfidfMatrix)
+  var testDocuments = List("the cat sat on my face",
+                           "the dog sat on my bed",
+                           "the chicken lies on my sheets")
+  var matrix = TopicModel.term_by_document_matrix(testDocuments)
+  var tfidfMatrix = TopicModel.tf_idf_scores(matrix, testDocuments)
+  var str = TopicModel.create_object_for_LSA_model(tfidfMatrix)
 
+  println("term2: ")
+  println(TopicModel.getTermByNumber(2))
+
+  println("document3: ")
+  println(TopicModel.getDocumentByNumber(2, testDocuments))
 
   /***********************    matrix on actual data   *************************/ 
-  var actual_matrix = TopicModel.term_by_document_matrix(reviews)
-  println("****finished creating term by doc matrix****")
+  // var actual_matrix = TopicModel.term_by_document_matrix(reviews)
+  // println("****finished creating term by doc matrix****")
 
-  var tfidfMatrix = TopicModel.tf_idf_scores(actual_matrix, reviews)
-  println("****finished creating tfid matrix****")
+  // var tfidfMatrix = TopicModel.tf_idf_scores(actual_matrix, reviews)
+  // println("****finished creating tfid matrix****")
 
-  var str = TopicModel.create_object_for_LSA_model(tfidfMatrix)
-  println("****finished creating object for LSA model matrix****")
+  // var str = TopicModel.create_object_for_LSA_model(tfidfMatrix)
+  // println("****finished creating object for LSA model matrix****")
 
 
   // Explanation of SVD: The A is our actual matrix, we call the reduced one "A2"
 
 
 
-   svd_rank_reduce_and_return_error(tfidfMatrix, reduced_rank)
+  // svd_rank_reduce_and_return_error(tfidfMatrix, reduced_rank)
 
 
 
